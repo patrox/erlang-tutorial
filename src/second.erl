63feacfb44e6684d399fb8b3f14a7be55eb42074
Patryk Najda <ptrknjd@gmail.com>
2017-03-04 16:32:09 +0100

    Add maxThree & howManyEquals functions

--- src/second.erl
+++ src/second.erl
@@ -1,6 +1,6 @@
 -module(second).
 
--export([hypotenuse/2, perimeter/2, area/2, xOr/2, xOr2/2, xOr3/2]).
+-export([hypotenuse/2, perimeter/2, area/2, xOr/2, xOr2/2, xOr3/2, maxThree/3, howManyEqual/3]).
 
 hypotenuse(A, B) ->
   C2 = first:square(A) + first:square(B),
@@ -22,3 +22,18 @@ xOr2(A, B) ->
 
 xOr3(A, B) ->
   A and not(B) or not(A) and B.
+
+maxThree(A, B, C) ->
+  MaxAB = max(A, B),
+  max(MaxAB, C).
+
+howManyEqual(A, A, A) ->
+  3;
+howManyEqual(A, A, _) ->
+  2;
+howManyEqual(A, _, A) ->
+  2;
+howManyEqual(_, A, A) ->
+  2;
+howManyEqual(_, _, _) ->
+  0.
