53a67e2a162af064384101992292d7c3940df8ee
Patryk Najda <ptrknjd@gmail.com>
2017-03-04 11:26:35 +0100

    Add second:perimeter function

--- src/second.erl
+++ src/second.erl
@@ -1,7 +1,11 @@
 -module(second).
 
--export([hypotenuse/2]).
+-export([hypotenuse/2, perimeter/2]).
 
 hypotenuse(A,B) ->
   C2 = first:square(A) + first:square(B),
   math:sqrt(C2).
+
+perimeter(A,B) ->
+  C = hypotenuse(A,B),
+  A + B + C.
