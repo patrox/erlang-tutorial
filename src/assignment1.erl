6c50c3a7f0a443f53912b2a7b07ff533da9942d2
Patryk Najda <ptrknjd@gmail.com>
2017-03-05 11:20:22 +0100

    Add enclose function

--- src/assignment1.erl
+++ src/assignment1.erl
@@ -1,5 +1,5 @@
 -module(assignment1).
--export([perimeter/1, area/1]).
+-export([perimeter/1, area/1, enclose/1, bits/1]).
 
 perimeter({circle, R})
   -> 2 * math:pi() * R;
@@ -12,6 +12,20 @@ area({circle, R})
   -> math:pi() * R * R;
 area({rectangle, H, W})
   -> H * W;
-area({triangle, A, B, _}) %assuming that triangle is a right angle triangle and A & B are his cathetuses
+%assuming that triangle is a right angle triangle and A & B are his cathetuses
+area({triangle, A, B, _})
   -> (A * B) / 2.
+
+%for a circle the smallest enclosing rectangle will be a square with a side equal to a circle diameter (2 * radius)
+enclose({circle, R})
+  -> {rectangle, 2 * R, 2 * R};
+%for a rectangle the smallest enclosing rectangle will the rectangle itself ...
+enclose({rectangle, H, W})
+  -> {rectangle, H, W};
+%assuming that triangle is a right angle triangle and A & B are his cathetuses
+enclose({triangle, A, B, _})
+  -> {rectangle, A, B}.
+
+bits(N) ->
+  0.
 
