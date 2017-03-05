069f353d99c0b41bb2086b6535e2715309c9b176
Patryk Najda <ptrknjd@gmail.com>
2017-03-05 18:13:38 +0100

    Add bits & bits_tail functions

--- src/assignment1.erl
+++ src/assignment1.erl
@@ -1,5 +1,5 @@
 -module(assignment1).
--export([perimeter/1, area/1, enclose/1, bits/1]).
+-export([perimeter/1, area/1, enclose/1, bits/1, bits_tail/1]).
 
 perimeter({circle, R})
   -> 2 * math:pi() * R;
@@ -19,13 +19,21 @@ area({triangle, A, B, _})
 %for a circle the smallest enclosing rectangle will be a square with a side equal to a circle diameter (2 * radius)
 enclose({circle, R})
   -> {rectangle, 2 * R, 2 * R};
-%for a rectangle the smallest enclosing rectangle will the rectangle itself ...
+%for a rectangle the smallest enclosing rectangle will be the same rectangle itself ...
 enclose({rectangle, H, W})
   -> {rectangle, H, W};
 %assuming that triangle is a right angle triangle and A & B are his cathetuses
 enclose({triangle, A, B, _})
   -> {rectangle, A, B}.
 
+bits(N) when N < 2 ->
+  N;
 bits(N) ->
-  0.
+  bits(N div 2) + bits(N rem 2).
 
+bits_tail(N) ->
+  bits_tail(N div 2, N rem 2, 0).
+bits_tail(D, R, A) when D < 2 ->
+  D + R + A;
+bits_tail(D, R, A) ->
+  bits_tail(D div 2, D rem 2, A + R).
