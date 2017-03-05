f69ecdc92a2ada7d02c1f5bcba10cdfb7323ec58
Patryk Najda <ptrknjd@gmail.com>
2017-03-05 11:07:18 +0100

    Add solutions for first part of Assignment #1

--- src/assignment1.erl
+++ src/assignment1.erl
@@ -1,13 +1,17 @@
-%%%-------------------------------------------------------------------
-%%% @author pnajda
-%%% @copyright (C) 2017, <COMPANY>
-%%% @doc
-%%%
-%%% @end
-%%% Created : 05. mar 2017 10:46
-%%%-------------------------------------------------------------------
 -module(assignment1).
--author("pnajda").
+-export([perimeter/1, area/1]).
 
-%% API
--export([]).
+perimeter({circle, R})
+  -> 2 * math:pi() * R;
+perimeter({rectangle, H, W})
+  -> 2 * (H + W);
+perimeter({triangle, A, B, C})
+  -> A + B + C.
+
+area({circle, R})
+  -> math:pi() * R * R;
+area({rectangle, H, W})
+  -> H * W;
+area({triangle, A, B, _}) %assuming that triangle is a right angle triangle and A & B are his cathetuses
+  -> (A * B) / 2.
+
